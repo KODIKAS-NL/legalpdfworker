6bff2c08ab9bef88b5a9e4bbebba1a3772708493
sven4ask <sven@legalthings.io>
2016-07-07 16:56:45 +0200

    Fix tests and small changes

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -4,40 +4,40 @@ const fs = require('fs');
 const Promise = require('bluebird');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './tmp/')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
+  destination: function(req, file, cb) {
+    cb(null, './tmp/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 })
 
 const upload = multer({
-    storage: storage
+  storage: storage
 })
 
 const router = express.Router();
 
 router.use(upload.single('file'), function(req, res, next) {
 
-    req.fileUpload = function() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(req.file.path, 'utf8', (err, data) => {
-                if (err) {
-                    reject({
-                        error: err
-                    });
-                } else {
-                    resolve({
-                        body: data,
-                        documentPath: req.file.path
-                    });
-                }
-            });
-        });
-    }
+  req.fileUpload = function() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(req.file.path, 'utf8', (err, data) => {
+        if (err) {
+          reject({
+            error: err
+          });
+        } else {
+          resolve({
+            body: data,
+            documentPath: req.file.path
+          });
+        }
+      });
+    });
+  }
 
-    return next();
+  return next();
 });
 
 
--- server.js
+++ server.js
@@ -6,24 +6,22 @@ const log = require('winston');
 const routes = require('./routes');
 const fileUpload = require('./middleware/fileUpload');
 
-log.add(log.transports.File, {
-    filename: config.Legal_Proxy_Converter_Log_File_Name
-});
-log.remove(log.transports.Console);
-
 const app = express();
 
 app.use(bodyParser.text({
-    type: 'text/html'
+  type: 'text/html'
 }));
 app.use(fileUpload);
 
 //Routes
 app.use('/', routes);
+app.use((req, res) => {
+  res.send(404, 'Not found');
+});
 
 //create server and set listening port
 const server = app.listen(config.Legal_Proxy_Converter_Listener_Port, () => {
-    log.info(`Legal Converter listening on port  ${ server.address().port}`);
-})
+  log.info(`Legal Converter listening on port  ${ server.address().port}`);
+});
 
 module.exports = app;
